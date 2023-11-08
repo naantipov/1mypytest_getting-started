c4f19372f5128c4a4b80a734e77f9525877090af
a4a2n <a4a2n9@gmail.com>
2023-11-08 19:58:20 +0300

    поправил

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -2,6 +2,7 @@ from selene.support.shared import browser
 from selene import be, have
 import pytest
 
+
 @pytest.fixture
 def browser_configs():
     browser.config.window_width = 1690
@@ -14,6 +15,7 @@ def test_google_find_selene(browser_configs):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
 
 def test_google_find_fdghsjkdhsfk(browser_configs):
     browser.open('https://google.com')

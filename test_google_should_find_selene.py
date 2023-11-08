7f6695d389b8b5e0cc836f3107d347dd69f1bdfd
a4a2n <a4a2n9@gmail.com>
2023-11-08 18:45:15 +0300

    теперь робит, уиии!!!)))

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -7,18 +7,18 @@ import pytest
 @pytest.fixture
 def browser_configs():
     browser.config.window_width = 300
-    browser.config.window_height = 600
+    browser.config.window_height = 300
     browser.config.browser_name = 'chrome'
 
     yield
     browser.quit()
 
-def test_google_find_selene():
+def test_google_find_selene(browser_configs):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
-def test_google_find_fdghsjkdhsfk():
+def test_google_find_fdghsjkdhsfk(browser_configs):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('fdghsjkdhsfk').press_enter()
     browser.element('[class="card-section"]').should(have.text('По запросу fdghsjkdhsfk ничего не найдено'))

d3840ea6dc9822d53a62a1a7ca7c299c8c1cc224
a4a2n <a4a2n9@gmail.com>
2023-11-08 19:57:08 +0300

    поправил немного

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -2,12 +2,10 @@ from selene.support.shared import browser
 from selene import be, have
 import pytest
 
-
-
 @pytest.fixture
 def browser_configs():
-    browser.config.window_width = 800
-    browser.config.window_height = 800
+    browser.config.window_width = 1690
+    browser.config.window_height = 1080
     yield
     browser.quit()
 
@@ -21,11 +19,4 @@ def test_google_find_fdghsjkdhsfk(browser_configs):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('fdghsjkdhsfk').press_enter()
     browser.element('[class="card-section"]').should(have.text('По запросу fdghsjkdhsfk ничего не найдено'))
-
-
-
-
-
-
-
 
